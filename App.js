41beb13ed636fe3c7c91b0c5a79a7966cb62eca4
Lucas Pinheiro <lucaspinheiroalberdanha@gmail.com>
2022-03-23 14:05:50 -0300

    first commit

--- App.js
+++ App.js
@@ -1,12 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
+
 import React from 'react';
+
 import { StyleSheet, Text, View } from 'react-native';
+
+import Card from './components/card';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <StatusBar style="light" />
+
+      <Text style={styles.title}>Meu App</Text>
+
+      <Card />
     </View>
   );
 }
@@ -14,8 +21,13 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#2A2A2A',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  title: {
+    color: '#fff',
+    fontSize: 24,
+    marginBottom: 24
+  }
 });
